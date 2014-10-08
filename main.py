f5db29a564c70cb0c6a7e399faf56bdcfb4a9981
Eric Feng <erichfeng@gmail.com>
2014-10-07 20:00:35 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from elasticsearch import Elasticsearch
+
+app = Flask(__name__)
+es = Elasticsearch()
+
+# Query object
+
+@app.route("/")
+def main():
+	return "Hello World!"
+
+if __name__ == "__main__":
+	app.run()
