d862db763fe5f6047a30fcbcdc12319dd4cd7a75
Stephanie Wang <stephwang@dropbox.com>
2014-10-19 00:00:26 -0700

    case/if statements and nested math expressions should work now

--- queryparser.py
+++ queryparser.py
@@ -8,40 +8,16 @@
 # - joins (add join keyword) x
 # - subqueries (recurse selectStmt) x
 # - support for arithmetic expressions in columns (ex. col1 + col2) x
-# - support for if/case/etc. statements
+# - support for if/case/etc. statements x
 # - group by / having x
 #
 from pyparsing import Literal, CaselessLiteral, Word, Upcase, delimitedList, Optional, \
     Combine, Group, alphas, nums, alphanums, ParseException, Forward, oneOf, quotedString, \
     ZeroOrMore, restOfLine, Keyword, SkipTo, OneOrMore
 
-# define SQL tokens
-selectStmt = Forward()
-selectToken = Keyword("select", caseless=True)
-fromToken = Keyword("from", caseless=True)
-asToken = Keyword("as", caseless=True)
-
-columnName = Forward()
-colIdent = Upcase(Word( alphanums + "_$()" ).setName("column identifier"))
-
-arithOp  = Word( "+-*/", max=1 )   # arithmetic operators
-
-columnName << (delimitedList( colIdent, ".", combine=True )
-				+ ZeroOrMore(arithOp + delimitedList( colIdent, ".", combine=True )))
-
-columnNameList = delimitedList( columnName.setResultsName("columns", listAllMatches=True)
-				+ Optional(asToken + colIdent) )
-
-tableIdent = Word( alphas, alphanums + "_$" ).setName("table identifier")
-tableName = Upcase( delimitedList( tableIdent, ".", combine=True ) )
-
-whereExpression = Forward()
-havingExpression = Forward()
-and_ = Keyword("and", caseless=True)
-or_ = Keyword("or", caseless=True)
-in_ = Keyword("in", caseless=True)
-
+# define base types and operators
 E = CaselessLiteral("E")
+arithop  = Word( "+-*/", max=1 )   # arithmetic operators
 binop = oneOf("= != < > >= <= eq ne lt le gt ge", caseless=True)
 arithSign = Word("+-",exact=1)
 realNum = Combine( Optional(arithSign) + ( Word( nums ) + "." + Optional( Word(nums) )  |
@@ -50,12 +26,39 @@ realNum = Combine( Optional(arithSign) + ( Word( nums ) + "." + Optional( Word(n
 intNum = Combine( Optional(arithSign) + Word( nums ) +
             Optional( E + Optional("+") + Word(nums) ) )
 
-columnRval = realNum | intNum | Upcase(quotedString) | columnName # need to add support for alg expressions
+# define SQL tokens
+selectStmt = Forward()
+selectToken = Keyword("select", caseless=True)
+fromToken = Keyword("from", caseless=True)
+asToken = Keyword("as", caseless=True)
+
+colIdent = Upcase(Word( alphanums + "_$()." ).setName("column identifier"))
+columnName = Combine( ( CaselessLiteral("case when") + SkipTo(CaselessLiteral("end"), include=True) ) |
+               ( Optional("(") + colIdent + Optional("(") + ZeroOrMore((arithop|binop) + Optional("(") + colIdent + Optional(")")) + Optional(")") )
+             , adjacent=False )
+
+columnNameList = delimitedList( columnName.setResultsName("columns", listAllMatches=True)
+				+ Optional(asToken + colIdent) )
+
+tableName = Upcase(Word( alphanums + "_$." ).setName("table identifier"))
+
+whereExpression = Forward()
+havingExpression = Forward()
+and_ = Keyword("and", caseless=True)
+or_ = Keyword("or", caseless=True)
+in_ = Keyword("in", caseless=True)
+
+whereColumn = (( CaselessLiteral("case when") + SkipTo(CaselessLiteral("end"), include=True)) |
+               ( Optional("(") + colIdent + Optional("(") + ZeroOrMore(arithop + Optional("(") + colIdent + Optional(")")) + Optional(")") )
+              )
+
+columnRval = realNum | intNum | Upcase(quotedString) | whereColumn # need to add support for alg expressions
+
 condition = Group(
-  ( columnName + binop + columnRval ) |
-  ( columnName + in_ + "(" + delimitedList( columnRval ) + ")" ) |
-  ( columnName + in_ + "(" + selectStmt + ")" ) |
-  ( columnName + "between" + columnRval + "and" + columnRval) |
+  ( whereColumn + binop + columnRval ) |
+  ( whereColumn + in_ + "(" + delimitedList( columnRval ) + ")" ) |
+  ( whereColumn + in_ + "(" + selectStmt + ")" ) |
+  ( whereColumn + "between" + columnRval + "and" + columnRval) |
   ( "(" + whereExpression + ")" )
   )
 whereExpression << condition.setResultsName("where", listAllMatches=True) + ZeroOrMore( ( and_ | or_ ) + whereExpression )
@@ -79,4 +82,6 @@ queryToParse = selectStmt
 # define Oracle comment format, and ignore them
 oracleSqlComment = "--" + restOfLine
 queryToParse.ignore( oracleSqlComment )
+
+print queryToParse.parseString("select x+y/z from test.table where x + y/z > 1")
 
